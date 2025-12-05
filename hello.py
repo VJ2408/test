a0de3cd653d7fea66de9bcf33d0c257a17716d65
Vijai Adithya <vijai.adithya24@gmail.com>
2025-12-05 17:07:24 +0800

    F9nished testing

--- hello.py
+++ hello.py
@@ -9,8 +9,4 @@ b= 10
 while a<b:
     print("hello there, I am obiwan kenobi")
     a = a+1
-print("I am done")
-print("This is a new line added to the file.")
-print("Final line added for testing purposes.")
-print("End of the script.")
-print("This is another line added to the script.")
+print("finished testing for memory leaks")
