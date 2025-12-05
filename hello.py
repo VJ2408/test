df09bdeba53e86f20c7f0c949f7027623ad81257
Vijai Adithya <vijai.adithya24@gmail.com>
2025-12-05 16:57:38 +0800

    What

--- hello.py
+++ hello.py
@@ -5,7 +5,6 @@ while a<b:
     print("hello there, I am obiwan kenobi")
     a = a+1
 print("I am done")
-print("Goodbye now")
 print("This is a new line added to the file.")
 print("Final line added for testing purposes.")
 print("End of the script.")
